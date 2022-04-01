07c3ac7b2ea5d98874dcd6f71ce3dd2d0d4a5801
Lovro Markovic <lovro.markovic@fer.hr>
2022-04-01 12:03:58 +0200

    Add CI, update README

---
+++ .ci/ci_before_install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+distro=`lsb_release -r | awk '{ print $2 }'`
+[ "$distro" = "18.04" ] && ROS_DISTRO="melodic"
+[ "$distro" = "20.04" ] && ROS_DISTRO="noetic"
+
+echo "Starting install"
+
+# get the current commit SHA
+SHA=`git rev-parse HEAD`
+
+# get the current package name
+PACKAGE_NAME=${PWD##*/}
+
+sudo apt-get -y install git
+
+echo "clone uav_ros_stack"
+cd
+git clone https://github.com/larics/uav_ros_stack.git
+cd uav_ros_stack
+
+echo "running the main install.sh"
+./installation/install.sh
+
+gitman update --force
+
+# checkout the SHA
+cd ~/uav_ros_stack/.gitman/$PACKAGE_NAME
+git checkout "$SHA"
+
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src
+ln -s ~/uav_ros_stack
+source /opt/ros/$ROS_DISTRO/setup.bash
+cd ~/catkin_ws
+
+echo "install ended"
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# uav_ros_drivers
-A collection of the commonly used UAV equipment drivers for the 'uav_ros_stack' package.
+# UAV ROS drivers
+
+This packages integrates all driver components for the  [uav_ros_stack](https://github.com/lmark1/uav_ros_stack).  
+
+It serves as a collection of the commonly used UAV sensor and equipment drivers.
+
