b6fcca0ace5e68930af8880965e8024155c40cdf
Lovro Markovic <lovro.markovic@fer.hr>
2022-04-01 16:34:29 +0200

    Add installation script

---
+++ installation/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Exit immediatelly if a command exits with a non-zero status
+set -e
+
+# Executes a command when DEBUG signal is emitted in this script - should be after every line
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+
+# Executes a command when ERR signal is emmitted in this script
+trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
+
+distro=`lsb_release -r | awk '{ print $2 }'`
+[ "$distro" = "18.04" ] && ROS_DISTRO="melodic"
+[ "$distro" = "20.04" ] && ROS_DISTRO="noetic"
+
+sudo apt-get update
+sudo apt-get -y \
+  ros-$ROS_DISTRO-pcl-ros
